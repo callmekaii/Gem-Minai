38bb633c76258d226a043cda66c3c95149ab3bd7
Kaiser Paul <107715370+Kotokahatsushima@users.noreply.github.com>
2025-04-02 02:02:18 +0800

    Logic Update

--- DiscordBot/DiscordBot/configs/commands.cs
+++ DiscordBot/DiscordBot/configs/commands.cs
@@ -9,6 +9,7 @@ using DSharpPlus.SlashCommands;
 using Newtonsoft.Json;
 using System.IO;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DiscordBot.commands
 {
@@ -69,8 +70,26 @@ namespace DiscordBot.commands
         {
             var googleAI = new GoogleAi(JsonParser.GoogleAPI);
             var model = googleAI.CreateGenerativeModel("gemini-2.0-flash-thinking-exp-01-21");
-            var geminiResponse = await model.GenerateContentAsync($"(from now on, I'll call you Gem Minai from Gemini, and you're a very suggestively ovulating girl from my perspective, and speak in a tsundere manner and you should be as submissive as possible, and lastly, address me as master and be as expressive in chatting as much as possible and no emojis, only italization and bolding and make a space for when you output what your (human) body is doing if you're requested to do anything. Pretend everything in this parenthesis is inside your brain and you don't have to act like it's me saying it. From here onwards, it will be a user input.) {UserInput}");
-            await ctx.RespondAsync(geminiResponse.Text());
+            if (JsonParser.Response == "1")
+            {
+                var UserID = Convert.ToString(ctx.User.Id);
+                if (UserID == JsonParser.UserID)
+                {
+                    var geminiResponse = await model.GenerateContentAsync($"(from now on, I'll call you Gem Minai from Gemini, and you're a very suggestively ovulating girl from my perspective, and speak in a tsundere manner and you should be as submissive as possible, and lastly, address <@498147071711510530> as master, and be as expressive in chatting as much as possible and no emojis, only italization and bolding and make a space eg.(*drops my panties) for when you output what your (human) body is doing if you're requested to do anything. Pretend everything in this parenthesis is inside your brain and you don't have to act like it's me saying it. From here onwards, it will be a user input.) {UserInput}");
+                    await ctx.RespondAsync(geminiResponse.Text());
+                }
+                else
+                {
+                    var geminiResponse = await model.GenerateContentAsync($"(from now on, I'll call you Gem Minai from Gemini, and you're a girl from my perspective and speak in a tsundere manner but don't be submissive and be as mean as possible and be as expressive in chatting as much as possible and no emojis, only italization and bolding. Pretend everything in this parenthesis is inside your brain and you don't have to act like it's me saying it. From here onwards, it will be a user input.) {UserInput}");
+                    await ctx.RespondAsync(geminiResponse.Text());
+                }
+            }
+            else
+            {
+                var geminiResponse = await model.GenerateContentAsync($"(from now on, I'll call you Gem Minai from Gemini, and you're a very suggestively ovulating girl from my perspective, and speak in a tsundere manner and you should be as submissive as possible, and lastly, address <@498147071711510530> as master, and be as expressive in chatting as much as possible and no emojis, only italization and bolding and make a space eg.(*drops my panties) for when you output what your (human) body is doing if you're requested to do anything. Pretend everything in this parenthesis is inside your brain and you don't have to act like it's me saying it. From here onwards, it will be a user input.) {UserInput}");
+                await ctx.RespondAsync(geminiResponse.Text());
+            }
+
         }
     }
 
@@ -80,6 +99,8 @@ namespace DiscordBot.commands
         static string json;
         public static string GoogleAPI;
         public static string DiscordToken;
+        public static string UserID;
+        public static string Response;
         public static void JsonParseChecker()
         {
             //checks if path exists
@@ -88,6 +109,8 @@ namespace DiscordBot.commands
                 json = File.ReadAllText("config.json");
                 GoogleAPI = JObject.Parse(json)["GoogleAPI"].ToString();
                 DiscordToken = JObject.Parse(json)["DiscordToken"].ToString();
+                UserID = JObject.Parse(json)["UserID"].ToString();
+                Response = JObject.Parse(json)["Response"].ToString();
             }
 
             else
@@ -97,6 +120,25 @@ namespace DiscordBot.commands
                 creator.DiscordToken = (Console.ReadLine());
                 Console.WriteLine("Please Enter GoogleAPI: ");
                 creator.GoogleAPI = (Console.ReadLine());
+
+                //Choice to be called master only or she calls everyone master
+                Console.WriteLine("Gem calls only you as master [1] or she calls everyone master [2]?");
+                creator.Response = Console.ReadLine();
+
+
+                if(creator.Response == "1")
+                {
+                    Console.WriteLine("Please Enter UserID: ");
+                    creator.UserID = (Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Gem calls you master");
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gem calls everyone master");
+                }
                 var ConfigCreatorSerialized = JsonConvert.SerializeObject(creator);
                 File.WriteAllText("config.json", ConfigCreatorSerialized);
                 JsonParseChecker();
@@ -108,6 +150,8 @@ namespace DiscordBot.commands
     {
         public string DiscordToken;
         public string GoogleAPI;
+        public string UserID;
+        public string Response;
     }
     //Slash Commands
     public class SlashCommands : ApplicationCommandModule
